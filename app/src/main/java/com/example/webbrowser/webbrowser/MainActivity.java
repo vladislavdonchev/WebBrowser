2ac7a621f051b356638b5e14fea26cca366695bc
gabi <abvjd@gmail.com>
2017-06-15 12:35:56 +0300

    WebView logic moved to WebViewFragment class

--- app/src/main/java/com/example/webbrowser/webbrowser/MainActivity.java
+++ app/src/main/java/com/example/webbrowser/webbrowser/MainActivity.java
@@ -5,26 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher, View.OnTouchListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher {
 
     private EditText addressBarEditText;
     private Button addNewTabButton;
     private ImageButton menuButton;
     private Button goButton;
-    private ProgressBar webViewProgressBar;
+    private WebViewFragment activeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +31,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         goButton = (Button) findViewById(R.id.activity_main_go_button);
         goButton.setEnabled(false);
 
-        webViewProgressBar = (ProgressBar) findViewById(R.id.activity_main_progress_bar);
-        webViewProgressBar.setMax(100);
-        webViewProgressBar.setVisibility(View.GONE);
-
         addNewTabButton.setOnClickListener(this);
         menuButton.setOnClickListener(this);
         goButton.setOnClickListener(this);
 
         addressBarEditText.addTextChangedListener(this);
 
-        webView.setOnTouchListener(this);
-
         if (savedInstanceState != null) {
-            webView.restoreState(savedInstanceState);
             addressBarEditText.setText(savedInstanceState.getString(Constants.ADDRESS_BAR_TEXT_KEY, ""));
         }
     }
@@ -59,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(Constants.ADDRESS_BAR_TEXT_KEY, addressBarEditText.getText().toString());
-        webView.saveState(outState);
+        activeFragment.saveWebViewState(outState);
     }
 
     @Override
@@ -72,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(this, "Menu button clicked", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.activity_main_go_button:
-                loadURL(addressBarEditText.getText().toString());
+                activeFragment.loadURL(addressBarEditText.getText().toString());
                 break;
         }
 
@@ -81,42 +68,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    private void loadURL(String url) {
-        WebSettings wbset = webView.getSettings();
-        wbset.setJavaScriptEnabled(true);
-        webView.setWebViewClient(new MyWebViewClient());
-        webView.setWebChromeClient(new MyWebChromeClient());
-
-        webView.loadUrl(formattedURL(url));
+        activeFragment.onBackPressed();
     }
 
     private void hideKeyboard() {
         InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(
                 Context.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(addressBarEditText.getWindowToken(), 0);
-    }
-
-    private String formattedURL(String input) {
-        String formattedURL = input.toLowerCase();
-        if (!formattedURL.startsWith("http://") && !formattedURL.startsWith("https://")) {
-            formattedURL = "http://" + input;
-        }
-        return formattedURL;
-    }
-
-    private void updateProgressBar(int progress) {
-        webViewProgressBar.setProgress(progress);
-
-        if (progress>=100) {
-            webViewProgressBar.setVisibility(View.GONE);
-        }
     }
 
     @Override
@@ -132,32 +90,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void afterTextChanged(Editable editable) {
         goButton.setEnabled(editable.length() != 0);
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        hideKeyboard();
-        return false;
-    }
-
-    private class MyWebViewClient extends WebViewClient {
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            webViewProgressBar.setProgress(0);
-            webViewProgressBar.setVisibility(View.VISIBLE);
-
-            return false;
-        }
-    }
-
-    private class MyWebChromeClient extends WebChromeClient {
-
-        @Override
-        public void onProgressChanged(WebView view, int newProgress) {
-            super.onProgressChanged(view, newProgress);
-
-            MainActivity.this.updateProgressBar(newProgress);
-        }
     }
 }
--- app/src/main/java/com/example/webbrowser/webbrowser/WebViewFragment.java
+++ app/src/main/java/com/example/webbrowser/webbrowser/WebViewFragment.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * Created by Asus on 6/15/2017.
  */
 
-public class WebViewFragment extends Fragment {
+public class WebViewFragment extends Fragment implements View.OnTouchListener {
 
     private WebView webView;
+    private ProgressBar webViewProgressBar;
 
     @Nullable
     @Override
@@ -22,6 +28,77 @@ public class WebViewFragment extends Fragment {
         View content = inflater.inflate(R.layout.fragment_web_view, container);
         webView = (WebView) content.findViewById(R.id.fragment_web_view);
 
+        webViewProgressBar = (ProgressBar) content.findViewById(R.id.fragment_progress_bar);
+        webViewProgressBar.setMax(100);
+        webViewProgressBar.setVisibility(View.GONE);
+
+        webView.setOnTouchListener(this);
+
+        if (savedInstanceState != null) {
+            webView.restoreState(savedInstanceState);
+        }
+
         return content;
+    }
+
+    public void saveWebViewState(Bundle outState) {
+        webView.saveState(outState);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
+    }
+
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+    }
+
+    public void loadURL(String url) {
+        WebSettings wbset = webView.getSettings();
+        wbset.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new MyWebChromeClient());
+
+        webView.loadUrl(formattedURL(url));
+    }
+
+
+    private String formattedURL(String input) {
+        String formattedURL = input.toLowerCase();
+        if (!formattedURL.startsWith("http://") && !formattedURL.startsWith("https://")) {
+            formattedURL = "http://" + input;
+        }
+        return formattedURL;
+    }
+
+    private void updateProgressBar(int progress) {
+        webViewProgressBar.setProgress(progress);
+
+        if (progress>=100) {
+            webViewProgressBar.setVisibility(View.GONE);
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            webViewProgressBar.setProgress(0);
+            webViewProgressBar.setVisibility(View.VISIBLE);
+
+            return false;
+        }
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            updateProgressBar(newProgress);
+        }
     }
 }
