d2cfc5d35713e692cedae5e8ba02baa40d50f1dc
Vladislav <v.donchev@hardcodes.net>
2017-06-29 15:01:24 +0300

    UI updates for the bookmarks functionality.

--- app/src/main/java/com/example/webbrowser/webbrowser/TabsAlertDialog.java
+++ app/src/main/java/com/example/webbrowser/webbrowser/TabsAlertDialog.java
@@ -2,6 +2,8 @@ package com.example.webbrowser.webbrowser;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
@@ -11,8 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -22,7 +26,13 @@ import java.util.zip.Inflater;
  * Created by username on 19/06/2017.
  */
 
-public class TabsAlertDialog extends AlertDialog implements AdapterView.OnItemClickListener {
+public class TabsAlertDialog extends AlertDialog implements AdapterView.OnItemClickListener, View.OnClickListener {
+
+    private Button openTabsButton;
+    private Button bookmarksButton;
+
+    private ListView openTabsList;
+    private ListView bookmarksList;
 
     protected TabsAlertDialog(@NonNull Context context) {
         super(context);
@@ -34,17 +44,23 @@ public class TabsAlertDialog extends AlertDialog implements AdapterView.OnItemCl
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, titles);
         LayoutInflater inflater = LayoutInflater.from(context);
 
-        int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 192, context.getResources().getDisplayMetrics());
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height);
+        //int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 192, context.getResources().getDisplayMetrics());
+        //ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height);
 
-        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.tabs_list_view, null);
-        ListView listView = (ListView) layout.findViewById(R.id.tabs_list_view);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.tabs_list_view, null);
+        openTabsList = (ListView) layout.findViewById(R.id.tabs_list_view_open);
+        openTabsList.setAdapter(adapter);
+        openTabsList.setOnItemClickListener(this);
+        bookmarksList = (ListView) layout.findViewById(R.id.tabs_list_view_bookmarked);
+
+        openTabsButton = (Button) layout.findViewById(R.id.tabs_list_view_open_button);
+        bookmarksButton = (Button) layout.findViewById(R.id.tabs_list_view_bookmarked_button);
+
+        openTabsButton.setOnClickListener(this);
+        bookmarksButton.setOnClickListener(this);
 
         setView(layout);
-
-        setTitle("Open Tabs");
+        //setTitle("Open Tabs");
         setButton(AlertDialog.BUTTON_NEGATIVE, "Close", (OnClickListener) null);
     }
 
@@ -55,5 +71,23 @@ public class TabsAlertDialog extends AlertDialog implements AdapterView.OnItemCl
         getContext().sendBroadcast(intent);
 
         dismiss();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tabs_list_view_open_button:
+                openTabsList.setVisibility(View.VISIBLE);
+                bookmarksList.setVisibility(View.GONE);
+                openTabsButton.setBackgroundResource(android.R.color.transparent);
+                bookmarksButton.setBackgroundResource(android.R.color.darker_gray);
+                break;
+            case R.id.tabs_list_view_bookmarked_button:
+                openTabsList.setVisibility(View.GONE);
+                bookmarksList.setVisibility(View.VISIBLE);
+                openTabsButton.setBackgroundResource(android.R.color.darker_gray);
+                bookmarksButton.setBackgroundResource(android.R.color.transparent);
+                break;
+        }
     }
 }
